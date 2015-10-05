7442df5a0e54af880f550e72e1b10ee98e17fca7
Ed Ceaser <ceasere@gmail.com>
2015-10-04 23:24:00 -0700

    Emit all follower events

--- extension.js
+++ extension.js
@@ -5,14 +5,14 @@ const POLL_INTERVAL = 30*1000;
 function Followers(nodecg) {
   this.nodecg = nodecg;
   this.twitch = nodecg.extensions['lfg-twitchapi'];
-  this.latestFollower = nodecg.Replicant("latestFollower", {defaultValue: null, persistent: false});
+  this.latestFollower = nodecg.Replicant("latestFollower", { defaultValue: null, persistent: true });
 
   this._scheduleFollowers();
 }
 
 Followers.prototype._scheduleFollowers = function() {
   this.nodecg.log.debug("Polling for TwitchTV Followers.");
-  this.twitch.get('/channels/{{username}}/follows', { limit: 1, direction: 'desc' }, (err, code, body) => {
+  this.twitch.get('/channels/{{username}}/follows', { limit: 50, direction: 'desc' }, (err, code, body) => {
     if (err) {
       this.nodecg.log.error(err);
       setTimeout(() => { this._scheduleFollowers() }, POLL_INTERVAL);
@@ -25,14 +25,26 @@ Followers.prototype._scheduleFollowers = function() {
       return;
     }
 
+    var lastFollowerTs = 0;
+    if (this.latestFollower.value) {
+      lastFollowerTs = Date.parse(this.latestFollower.value.created_at);
+    }
+
     if (body.follows.length > 0) {
       this.nodecg.log.debug("Discovered " + body.follows.length + " followers.");
-      this.latestFollower.value = body.follows[0].user.display_name;
+      this.latestFollower.value = body.follows[0];
+
+      for (var follower of body.follows.reverse()) {
+        var parsedTs = Date.parse(follower.created_at);
+        if (parsedTs > lastFollowerTs) {
+          this.nodecg.sendMessage('follower', follower);
+        }
+      }
     }
 
     setTimeout(() => { this._scheduleFollowers() }, POLL_INTERVAL);
   });
-}
+};
 
 module.exports = function(api) {
   return new Followers(api);
