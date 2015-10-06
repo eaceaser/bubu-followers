77366922464c7cf4c48fe9a34c646d51604277c5
Ed Ceaser <ceasere@gmail.com>
2015-10-05 21:01:07 -0700

    get rid of all useful syntax :(

--- extension.js
+++ extension.js
@@ -12,38 +12,40 @@ function Followers(nodecg) {
 
 Followers.prototype._scheduleFollowers = function() {
   this.nodecg.log.debug("Polling for TwitchTV Followers.");
-  this.twitch.get('/channels/{{username}}/follows', { limit: 50, direction: 'desc' }, (err, code, body) => {
-    if (err) {
-      this.nodecg.log.error(err);
-      setTimeout(() => { this._scheduleFollowers() }, POLL_INTERVAL);
-      return;
-    }
-
-    if (code != 200) {
-      this.nodecg.log.error("Unknown response code: "+code);
-      setTimeout(() => { this._scheduleFollowers() }, POLL_INTERVAL);
-      return;
-    }
-
-    var lastFollowerTs = 0;
-    if (this.latestFollower.value) {
-      lastFollowerTs = Date.parse(this.latestFollower.value.created_at);
-    }
-
-    if (body.follows.length > 0) {
-      this.nodecg.log.debug("Discovered " + body.follows.length + " followers.");
-      this.latestFollower.value = body.follows[0];
-
-      for (var follower of body.follows.reverse()) {
-        var parsedTs = Date.parse(follower.created_at);
-        if (parsedTs > lastFollowerTs) {
-          this.nodecg.sendMessage('follower', follower);
-        }
+  this.twitch.get('/channels/{{username}}/follows', { limit: 50, direction: 'desc' },
+    function (err, code, body) {
+      if (err) {
+        this.nodecg.log.error(err);
+        setTimeout(function() { this._scheduleFollowers() }.bind(this), POLL_INTERVAL);
+        return;
       }
-    }
 
-    setTimeout(() => { this._scheduleFollowers() }, POLL_INTERVAL);
-  });
+      if (code != 200) {
+        this.nodecg.log.error("Unknown response code: "+code);
+        setTimeout(function() { this._scheduleFollowers() }.bind(this), POLL_INTERVAL);
+        return;
+      }
+
+      var lastFollowerTs = 0;
+      if (this.latestFollower.value) {
+        lastFollowerTs = Date.parse(this.latestFollower.value.created_at);
+      }
+
+      if (body.follows.length > 0) {
+        this.nodecg.log.debug("Discovered " + body.follows.length + " followers.");
+        this.latestFollower.value = body.follows[0];
+
+        body.follows.reverse().map(function(follower) {
+          var parsedTs = Date.parse(follower.created_at);
+          if (parsedTs > lastFollowerTs) {
+            this.nodecg.sendMessage('follower', follower);
+          }
+        })
+      }
+
+      setTimeout(function() { this._scheduleFollowers() }.bind(this), POLL_INTERVAL);
+    }.bind(this)
+  );
 };
 
 module.exports = function(api) {
